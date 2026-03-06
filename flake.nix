73c4d6033846c983a5b712b1801442b207bb5ee7
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-07 05:49:09 +0900

    feat: add haskell-capnp 0.18 dependency for Cap'n Proto serialization
    Overlay capnp in flake.nix with doJailbreak (relaxes tight upper bounds
    for GHC 9.10), appendPatch (adds DuplicateRecordFields pragma to
    GenHelpers/Rpc.hs), and dontCheck (test suite has TH path issues in Nix
    sandbox). Also unmark/jailbreak transitive deps that are broken or
    bound-constrained on GHC 9.10: supervisors, lifetimes, network-simple,
    data-default-instances-vector.

--- flake.nix
+++ flake.nix
@@ -11,10 +11,32 @@
       let
         pkgs = nixpkgs.legacyPackages.${system};
 
+        # Patch for capnp: GHC 9.10 tightened DuplicateRecordFields rules
+        capnpDuplicateRecordFieldsPatch = pkgs.writeText "capnp-duplicate-record-fields.patch" ''
+          --- a/lib/Capnp/GenHelpers/Rpc.hs
+          +++ b/lib/Capnp/GenHelpers/Rpc.hs
+          @@ -1,3 +1,4 @@
+           {-# LANGUAGE DataKinds #-}
+          +{-# LANGUAGE DuplicateRecordFields #-}
+           {-# LANGUAGE FlexibleContexts #-}
+           {-# LANGUAGE TypeFamilies #-}
+        '';
+
         haskellPackages = pkgs.haskellPackages.override {
           overrides = hself: hsuper: {
             tank = hself.callCabal2nix "tank" ./. {};
             tank-layout = hself.callCabal2nix "tank-layout" ./tank-layout {};
+
+            # capnp 0.18: relax bounds for GHC 9.10, add DuplicateRecordFields pragma
+            capnp = pkgs.haskell.lib.dontCheck (pkgs.haskell.lib.appendPatch
+              (pkgs.haskell.lib.doJailbreak hsuper.capnp)
+              capnpDuplicateRecordFieldsPatch);
+
+            # capnp transitive deps: broken or tight upper bounds on GHC 9.10
+            supervisors = pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.unmarkBroken hsuper.supervisors);
+            lifetimes = pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.unmarkBroken hsuper.lifetimes);
+            network-simple = pkgs.haskell.lib.doJailbreak hsuper.network-simple;
+            data-default-instances-vector = pkgs.haskell.lib.doJailbreak (pkgs.haskell.lib.unmarkBroken hsuper.data-default-instances-vector);
           };
         };
 
