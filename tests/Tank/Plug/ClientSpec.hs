42a61a4d0a88648af37f10cec12f28e87ef833e8
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-07 15:55:19 +0900

    test: add plug client integration test for output broadcast

--- tests/Tank/Plug/ClientSpec.hs
+++ tests/Tank/Plug/ClientSpec.hs
@@ -5,11 +5,13 @@ import Test.Hspec
 import Control.Concurrent (forkIO, threadDelay, killThread)
 import Data.Maybe (isNothing)
 import qualified Data.Set as Set
+import Data.UUID.V4 (nextRandom)
 import System.IO.Temp (withSystemTempDirectory)
 
-import Tank.Core.Types (PlugCapability(..))
+import Tank.Core.Types (CellId(..), PlugCapability(..))
+import Tank.Core.Protocol (Message(..), MessageEnvelope(..), Target(..))
 import Tank.Daemon.Main (startDaemonAt)
-import Tank.Plug.Client (connectDaemon, disconnectPlug, pcPlugId)
+import Tank.Plug.Client (connectDaemon, sendMsg, recvMsg, disconnectPlug, pcPlugId)
 
 spec :: Spec
 spec = describe "Plug.Client" $ do
@@ -33,3 +35,51 @@ spec = describe "Plug.Client" $ do
       let sockPath = dir ++ "/nonexistent.sock"
       result <- connectDaemon sockPath "terminal" (Set.singleton CapTerminal)
       result `shouldSatisfy` isNothing
+
+  it "client creates cell and broadcasts output to second client" $ do
+    withSystemTempDirectory "tank-client" $ \dir -> do
+      let sockPath = dir ++ "/test.sock"
+      daemonThread <- forkIO $ startDaemonAt sockPath
+      threadDelay 200000
+
+      -- Connect terminal plug (client 1)
+      Just client1 <- connectDaemon sockPath "terminal" (Set.singleton CapTerminal)
+
+      -- Connect observer plug (client 2)
+      Just client2 <- connectDaemon sockPath "observer" Set.empty
+
+      let pid1 = pcPlugId client1
+          pid2 = pcPlugId client2
+
+      -- Client 1 creates a cell
+      cuid <- nextRandom
+      let cid = CellId cuid
+      sendMsg client1 $ MessageEnvelope 1 pid1 TargetBroadcast 0
+        (MsgCellCreate cid "/tmp")
+      threadDelay 50000
+
+      -- Both attach
+      sendMsg client1 $ MessageEnvelope 1 pid1 TargetBroadcast 0
+        (MsgCellAttach cid pid1)
+      sendMsg client2 $ MessageEnvelope 1 pid2 TargetBroadcast 0
+        (MsgCellAttach cid pid2)
+      threadDelay 50000
+
+      -- Client 1 sends output
+      sendMsg client1 $ MessageEnvelope 1 pid1 TargetBroadcast 0
+        (MsgOutput cid "hello world")
+
+      -- Both should receive broadcast
+      resp1 <- recvMsg client1
+      case resp1 of
+        Right env -> mePayload env `shouldBe` MsgOutput cid "hello world"
+        Left err  -> expectationFailure $ "client1 broadcast failed: " ++ err
+
+      resp2 <- recvMsg client2
+      case resp2 of
+        Right env -> mePayload env `shouldBe` MsgOutput cid "hello world"
+        Left err  -> expectationFailure $ "client2 broadcast failed: " ++ err
+
+      disconnectPlug client1
+      disconnectPlug client2
+      killThread daemonThread
