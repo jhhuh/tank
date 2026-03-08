c72752cbe987dc7cafb1a922b7f3ccc3ab80f31f
Ji-Haeng Huh <jhhuh.note@gmail.com>
2026-03-08 15:15:43 +0900

    feat: add GridDelta domain types matching Cap'n Proto schema

--- src/Tank/Core/Types.hs
+++ src/Tank/Core/Types.hs
@@ -4,13 +4,20 @@ module Tank.Core.Types
   , Cell(..)
   , PlugCapability(..)
   , PlugInfo(..)
+  , GridDelta(..)
+  , CellUpdate(..)
+  , ViewportUpdate(..)
+  , EpochUpdate(..)
+  , GridSnapshot(..)
   ) where
 
 import Data.Map.Strict (Map)
 import Data.Set (Set)
 import Data.Text (Text)
 import Data.UUID (UUID)
-import Tank.Terminal.Grid (Grid)
+import Data.Word (Word64)
+import Tank.Core.CRDT (ReplicaId)
+import Tank.Terminal.Grid (Grid, GridCell)
 
 newtype CellId = CellId UUID
   deriving (Eq, Ord, Show)
@@ -39,3 +46,41 @@ data Cell = Cell
   , cellPtyOwner  :: !(Maybe PlugId)
   , cellGrid      :: !Grid
   } deriving (Show)
+
+data GridDelta
+  = DeltaCells ![CellUpdate]
+  | DeltaViewport !ViewportUpdate
+  | DeltaEpoch !EpochUpdate
+  | DeltaSnapshot !GridSnapshot
+  deriving (Eq, Show)
+
+data CellUpdate = CellUpdate
+  { cuAbsLine   :: !Word64
+  , cuCol       :: !Int
+  , cuCell      :: !GridCell
+  , cuEpoch     :: !Word64
+  , cuTimestamp  :: !Word64
+  , cuReplicaId :: !ReplicaId
+  } deriving (Eq, Show)
+
+data ViewportUpdate = ViewportUpdate
+  { vuAbsLine   :: !Word64
+  , vuTimestamp  :: !Word64
+  , vuReplicaId  :: !ReplicaId
+  } deriving (Eq, Show)
+
+data EpochUpdate = EpochUpdate
+  { euEpoch     :: !Word64
+  , euTimestamp  :: !Word64
+  , euReplicaId  :: !ReplicaId
+  } deriving (Eq, Show)
+
+data GridSnapshot = GridSnapshot
+  { gsWidth       :: !Int
+  , gsHeight      :: !Int
+  , gsBufferAbove :: !Int
+  , gsBufferBelow :: !Int
+  , gsViewport    :: !Word64
+  , gsEpoch       :: !Word64
+  , gsCells       :: ![CellUpdate]
+  } deriving (Eq, Show)
